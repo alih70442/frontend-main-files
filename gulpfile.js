913a47586228385194c8bd19b8b130deea5387bd
AliH <alih70442@gmail.com>
2021-11-12 17:20:41 +0330

    gulp --> build & dist seperated

--- gulpfile.js
+++ gulpfile.js
@@ -1,224 +1,210 @@
-'use strict';
+"use strict";
 
-// docs ==============================================================================================
-//
-// first run -> npm i -> to install all dependencies
-//
-// to finalize the project run --> gulp all
-//
-// to watch the project run --> gulp watch
-//
-// to show all tasks run --> gulp --tasks
-//
-// docs ==============================================================================================
-
-// import main gulp
-// need to install gulp-cli --> (npm i -g gulp-cli)
-// https://gulpjs.com/docs/en/getting-started/quick-start/
 var gulp = require('gulp');
 
-// css ===============================================
-{
-    // https://www.npmjs.com/package/gulp-sass
-    // https://www.npmjs.com/package/node-sass-import-once
-    // https://www.npmjs.com/package/node-sass
+const input_dir = 'dist/';
+const output_dir = 'build/';
 
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++ pug ++++++++++++++++++++++++++++++++++++++++++++++++ */
+{
+    // npm install gulp-pretty-html --save-dev
+
+    const { src, dest } = require('gulp');
+    const pug = require('gulp-pug');
+    const prettyHtml = require('gulp-pretty-html');
+
+    gulp.task('pug-build', function () {
+        return src('pug/pages/*.pug')
+            .pipe(
+                pug({
+                    pretty: true
+                })
+            )
+            .pipe(prettyHtml({
+                wrap_line_length: 160,
+                unformatted: ['code', 'pre', 'em', 'strong', 'b', 'br'],
+                extra_liners: ['span', '/span', 'a', 'i', 'img']
+            }))
+            .pipe(dest('pages/'));
+    });
+
+    gulp.task('pug:watch', function () {
+        gulp.watch('pug/*.pug', { delay: 1000 }, gulp.series(['pug-build']));
+    });
+}
+/* ++++++++++++++++++++++++++++++++++++++++++++++++ pug ++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++ css ++++++++++++++++++++++++++++++++++++++++++++++++ */
+{
     var sass = require('gulp-sass');
+    // var sassImportOnce = require('gulp-sass-import-once');
+    // var sass = require('node-sass');
     var importOnce = require('node-sass-import-once');
 
     sass.compiler = require('node-sass');
 
-    // sass +++
     gulp.task('sass', function () {
         return gulp
-            .src('dist/scss/**/*.scss')
+            .src(input_dir + 'scss/**/*.scss')
             .pipe(sass({ importer: importOnce, outputStyle: 'compressed' }).on('error', sass.logError))
-            .pipe(gulp.dest('dist/build/css/'))
+            .pipe(gulp.dest(output_dir + 'css/'))
     });
-    // sass +++
 
-    // other css +++
+    // postcss & autoprefixer +++
     {
-        // https://www.npmjs.com/package/gulp-cssnano
-        var cssnano = require('gulp-cssnano');
+        const autoprefixer = require('autoprefixer'),
+            postcss = require('gulp-postcss');
 
-        gulp.task('css-other', function () {
-            // input files
-            return gulp.src([
-                'dist/css/**/*.css'
-            ])
-                .pipe(cssnano())
-                .pipe(gulp.dest('dist/build/css/'));
-        });
+        gulp.task('css-prefix', function () {
+            return gulp
+                .src([
+                    output_dir + 'css/**/*.css',
+                    '!' + output_dir + 'css/libs/**/*'
+                ])
+                .pipe(postcss([autoprefixer()]))
+                .pipe(gulp.dest(output_dir + 'css/'))
+        })
+
+        gulp.task('autoprefixer', gulp.series('css-prefix'));
     }
-    // other css +++
+    // postcss & autoprefixer +++
 
     gulp.task('sass:watch', function () {
-        gulp.watch('dist/scss/**/*.scss', gulp.series(['sass']));
+        //   gulp.watch('dist/scss/**/*.scss', ['sass']);
+        gulp.watch(input_dir + 'scss/**/*.scss', gulp.series(['sass']));
     });
 
-    gulp.task('css-all', gulp.series('sass', 'css-other'));
+    gulp.task('css-other', function () {
+        return gulp
+            .src(input_dir + 'css/libs/**/*')
+            .pipe(gulp.dest(output_dir + 'css/libs/'))
+    });
+
+    gulp.task('css-build', gulp.series('sass', 'autoprefixer', 'css-other'));
 }
-// css ===============================================
+/* ++++++++++++++++++++++++++++++++++++++++++++++++ css ++++++++++++++++++++++++++++++++++++++++++++++++ */
 
-// js ================================================
+/* ++++++++++++++++++++++++++++++++++++++++++++++++ js ++++++++++++++++++++++++++++++++++++++++++++++++ */
 {
-    // https://www.npmjs.com/package/gulp-jshint
-    // https://www.npmjs.com/package/gulp-concat
-    // https://www.npmjs.com/package/gulp-uglify
-
-    // import dependencies
-    const jshint = require('gulp-jshint');
-    const concat = require('gulp-concat');
-    const uglify = require('gulp-uglify');
-
-    // jshing +++
-    {
-        gulp.task('js-hint', function () {
-            return gulp.src(['dist/js/**/*.js'])
-                .pipe(jshint())
-                .pipe(jshint.reporter('default'))
-                .pipe(jshint.reporter('fail'));
-        });
-    }
-    // jshing +++
-
-    gulp.task('js-temp', function () {
-        return gulp.src('dist/js/templates/*.js')
-            .pipe(concat('templates.js'))
-            .pipe(uglify())
-            .pipe(gulp.dest('dist/build/js/templates/'));
-    });
+    var concat = require('gulp-concat');
+    var uglify = require('gulp-uglify');
+    var ts = require('gulp-typescript');
+    var jsImport = require('gulp-js-import');
 
     gulp.task('js-pages', function () {
-        return gulp.src('dist/js/pages/*.js')
+        return gulp.src(input_dir + 'js/pages/*.js')
+            .pipe(jsImport({
+                hideConsole: true,
+                importStack: true,
+                es6import: true
+            }))
             .pipe(uglify())
-            .pipe(gulp.dest('dist/build/js/pages/'));
+            .pipe(gulp.dest(output_dir + 'js/pages/'));
     });
 
     gulp.task('js-public', function () {
-        return gulp.src('dist/js/public/public.js')
+        return gulp.src([input_dir + 'js/public/*.js'])
+            .pipe(jsImport({
+                hideConsole: true,
+                importStack: true,
+                es6import: true
+            }))
             .pipe(uglify())
-            .pipe(gulp.dest('dist/build/js/public/'));
+            .pipe(concat('public.js'))
+            .pipe(gulp.dest(output_dir + 'js/public/'));
     });
 
-    // other js +++
-    {
-        gulp.task('js-other', function () {
-            return gulp.src('dist/js/libs/**/*.js')
-                .pipe(uglify())
-                .pipe(gulp.dest('dist/build/js/'));
-        });
-    }
-    // other js +++
+    gulp.task('ts-build', function () {
+        return gulp.src([
+            output_dir + 'js/**/*.js',
+            '!' + output_dir + 'js/libs/*.js'
+        ])
+            .pipe(ts({
+                target: 'ES5',
+                noImplicitAny: true,
+                allowJs: true,
+                removeComments: true,
+                outDir: output_dir + 'js/'
+            }))
+            .pipe(uglify())
+            .pipe(gulp.dest(output_dir + 'js/'));
+    });
 
-    gulp.task('js-all', gulp.series('js-temp', 'js-pages', 'js-public', 'js-other'));
+    gulp.task('js-other', function () {
+        return gulp
+            .src(input_dir + 'js/libs/**/*')
+            .pipe(gulp.dest(output_dir + 'js/libs/'))
+    });
+
+    gulp.task('js-build', gulp.series('js-pages', 'js-public', 'ts-build', 'js-other'));
 
     gulp.task('js:watch', function () {
-        gulp.watch('dist/js/templates/*.js', gulp.series(['js-temp']));
-        gulp.watch('dist/js/pages/*.js', gulp.series(['js-pages']));
-        gulp.watch('dist/js/public/*.js', gulp.series(['js-public']));
+        gulp.watch([input_dir + 'js/templates/*.js', input_dir + 'js/public/classes/*.js'], gulp.series(['js-pages', 'js-public']));
+        gulp.watch(input_dir + 'js/pages/*.js', gulp.series(['js-pages']));
+        gulp.watch([input_dir + 'js/public/*.js'], gulp.series(['js-public']));
     });
 }
-// js ================================================
+/* ++++++++++++++++++++++++++++++++++++++++++++++++ js ++++++++++++++++++++++++++++++++++++++++++++++++ */
 
-// fontIcon ==========================================
-{
-    // https://www.npmjs.com/package/gulp-iconfont
-    // https://www.npmjs.com/package/gulp-iconfont-css
 
-    // import dependencies
-    var iconfont = require('gulp-iconfont');
-    var iconfontCss = require('gulp-iconfont-css');
-    var runTimestamp = Math.round(Date.now() / 1000);
-
-    var fontName = 'fontIcon';
-
-    gulp.task('fontIcon', function () {
-        return gulp.src(['dist/icons/*.svg'])
-            .pipe(iconfontCss({
-                fontName: fontName,
-                path: 'dist/icons/templates/template.css',
-                targetPath: '../../css/icons.css',
-                fontPath: '../fonts/'
-            }))
-            .pipe(iconfont({
-                fontName: fontName, // required
-                prependUnicode: true, // recommended option
-                formats: ['eot', 'woff', 'ttf', 'woff2', 'svg'], // default, 'woff2' and 'svg' are available
-                timestamp: runTimestamp, // recommended to get consistent builds when watching files
-                fixedWidth: false,
-                centerHorizontally: true,
-                normalize: true,
-            }))
-            .on('glyphs', function (glyphs, options) {
-                // CSS templating, e.g.
-                console.log(glyphs, options);
-            })
-            .pipe(gulp.dest('dist/build/fontIcons/'));
-    });
-
-    // other fontIcons +++
-    {
-        gulp.task('fontIcon-other', function () {
-            return gulp.src('dist/fontIcons/**/*')
-                .pipe(gulp.dest('dist/build/fontIcons/js/'));
-        });
-    }
-    // other fontIcons +++
-
-    // icon.css created by fontIcon, should concatinate to style.min.css (to apply)
-    gulp.task('fontIcon-all', gulp.series('fontIcon', 'fontIcon-other'));
-}
-// fontIcon ==========================================
-
-// image =============================================
+/* ++++++++++++++++++++++++++++++++++++++++++++++++ image ++++++++++++++++++++++++++++++++++++++++++++++++ */
 {
     // https://www.npmjs.com/package/gulp-imagemin
 
     // import dependencies
-    const imagemin = require('gulp-imagemin');
+    // const imagemin = require('gulp-imagemin');
 
-    gulp.task('image', function () {
-        gulp.src('dist/images/**/*.{png,jpg,svg,gif}')
-            .pipe(imagemin([
-                imagemin.gifsicle({ interlaced: true }),
-                imagemin.mozjpeg({ quality: 30, progressive: true }),
-                imagemin.optipng({ optimizationLevel: 5 }),
-                imagemin.svgo({
-                    plugins: [
-                        { removeViewBox: true },
-                        { cleanupIDs: false }
-                    ]
-                })
-            ]))
-            .pipe(gulp.dest('dist/build/images/'))
-    })
+    // gulp.task('images', function () {
+    //     gulp.src(input_dir + 'images/**/*.{png,jpg,svg,gif}')
+    //         .pipe(imagemin([
+    //             imagemin.gifsicle({ interlaced: true }),
+    //             imagemin.mozjpeg({ quality: 30, progressive: true }),
+    //             imagemin.optipng({ optimizationLevel: 5 }),
+    //             imagemin.svgo({
+    //                 plugins: [
+    //                     { removeViewBox: true },
+    //                     { cleanupIDs: false }
+    //                 ]
+    //             })
+    //         ]))
+    //         .pipe(gulp.dest(output_dir + 'images/'))
+    // })
 
-    gulp.task('image-all', gulp.series('image'));
+    // gulp.task('images-build', gulp.series('images'));
 }
-// image =============================================
+/* ++++++++++++++++++++++++++++++++++++++++++++++++ image ++++++++++++++++++++++++++++++++++++++++++++++++ */
 
-// other files =======================================
+/* ++++++++++++++++++++++++++++++++++++++++++++++++ other files ++++++++++++++++++++++++++++++++++++++++++++++++ */
 {
     gulp.task('fonts', function () {
-        return gulp.src('dist/fontIcons/**/*')
-            .pipe(gulp.dest('dist/build/fontIcons/js/'));
+        return gulp.src(input_dir + 'fonts/**/*')
+            .pipe(gulp.dest(output_dir + 'fonts/'));
     });
 
-    gulp.task('files-all', gulp.series('fonts'));
-}
-// other files =======================================
+    gulp.task('images', function () {
+        return gulp.src(input_dir + 'images/**/*')
+            .pipe(gulp.dest(output_dir + 'images/'));
+    });
 
-// watch files
+    gulp.task('files-build', gulp.series('fonts', 'images'));
+}
+/* ++++++++++++++++++++++++++++++++++++++++++++++++ other files ++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+// watch the project
 gulp.task('watch', function () {
-    // css
-    gulp.watch('dist/scss/**/*.scss', gulp.series(['sass']));
+    // pug
+    gulp.watch('pug/**/*.pug', { delay: 1000 }, gulp.series(['pug-build']));
+
+    // scss
+    gulp.watch(input_dir + 'scss/**/*.scss', gulp.series(['sass']));
+
     // js
-    gulp.watch('dist/js/templates/*.js', gulp.series(['js-temp']));
-    gulp.watch('dist/js/pages/*.js', gulp.series(['js-pages']));
-    gulp.watch('dist/js/public/*.js', gulp.series(['js-public']));
+    gulp.watch([input_dir + 'js/templates/*.js', input_dir + 'js/public/classes/*.js'], gulp.series(['js-pages', 'js-public']));
+    gulp.watch(input_dir + 'js/pages/*.js', gulp.series(['js-pages']));
+    gulp.watch(input_dir + 'js/public/*.js', gulp.series(['js-public']));
 });
 
-// build finalized project
-gulp.task('build', gulp.series('css-all', 'js-all', 'fontIcon-all', 'files-all', 'image-all'));
+// build the project
+gulp.task('build', gulp.series('pug-build', 'css-build', 'js-build', 'files-build'/*, 'images-build'*/));
